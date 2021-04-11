9bed80e20ffe4f990f755f627601ec3223d2e129
Leanne Sall <lsall1@my.bcit.ca>
2021-04-11 13:48:22 -0700

    added new packages signin/up complete backend

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   env: {
-    MONGO_URI: "",
+    MONGO_URI:
+      "mongodb+srv://leannesall:8XCzFFFPbxziQHIc@revopro.hujcp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    JWT_SECRET: "leanneistheverybesteveromg",
   },
 };
+
+// Initialize Firebase
