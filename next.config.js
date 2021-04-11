7d3ceae4d3af9514166146d5622d06366355c8bb
Leanne Sall <lsall1@my.bcit.ca>
2021-04-11 13:49:47 -0700

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  env: {
-    MONGO_URI:
-      "mongodb+srv://leannesall:8XCzFFFPbxziQHIc@revopro.hujcp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    JWT_SECRET: "leanneistheverybesteveromg",
-  },
-};
-
-// Initialize Firebase
