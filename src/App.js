06a41e4cce9a78c3365e86cedfb55aa141b14323
Leanne Sall <lsall1@my.bcit.ca>
2021-04-12 13:01:42 -0700

    finished auth and login/signup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-import SignUp from "./components/signUp";
-
-import { useAuthState } from "react-firebase-hooks/auth";
+import PrivateRoute from "./routes/PrivateRoute";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Landing from "./pages/Landing";
+import firebase from "firebase/app";
+import { AuthProvider } from "./hooks/useAuth";
+import fireStart from "./config/firebase";
 
 function App() {
   return (
-    <div>
-      <SignUp />
-    </div>
+    <AuthProvider>
+      <Router>
+        <Switch>
+          <PrivateRoute path="/dashboard">
+            <Dashboard />
+          </PrivateRoute>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/">
+            <Landing />
+          </Route>
+        </Switch>
+      </Router>
+    </AuthProvider>
   );
 }
 
