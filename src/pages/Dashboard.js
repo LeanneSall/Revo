88641a6060f9da470bd90d1114a52c5266baf500
Leanne Sall <lsall1@my.bcit.ca>
2021-04-12 15:15:58 -0700

    added a bunch of styling

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,9 +1,22 @@
-import { Button } from "@material-ui/core";
+import { Button, Container, Paper } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 import firebase from "firebase/app";
 import React from "react";
 import { useHistory } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
 import "firebase/auth";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+    "& > *": {
+      margin: theme.spacing(1),
+      width: theme.spacing(16),
+      height: theme.spacing(16),
+    },
+  },
+}));
 
 const Dashboard = () => {
   const history = useHistory();
@@ -12,10 +25,19 @@ const Dashboard = () => {
     signOut();
     history.push("/");
   };
+
+  const classes = useStyles();
   return (
     <>
-      <h1>Protected Route</h1>
-      <Button onClick={logout}>logout</Button>
+      <h1>Dashboard</h1>
+      <Container className={classes.root}>
+        //link to widgets as papers
+        <Paper elevation={3} />
+        <Paper elevation={3} />
+        <Paper elevation={3} />
+        //have bottom nav as android or hamburger for ios
+      </Container>
+      //Add add button at bottom of screen to add more widgets
     </>
   );
 };
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,10 +1,31 @@
+import { Container, Typography, Button, Link } from "@material-ui/core";
+import adventure from "../images/adventure.svg";
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 const Landing = () => {
+  const history = useHistory();
   return (
-    <div>
-      <h1>Landing Page</h1>
-    </div>
+    <Container maxWidth="sm">
+      <Typography variant="h1" align="center">
+        Revo
+      </Typography>
+      <div className="img">
+        <img className="adventure" src={adventure} />
+      </div>
+      <Typography variant="h4" align="center">
+        Reach Your Potential
+      </Typography>
+      <Typography variant="h5" align="center">
+        Revolutionize Your Life
+      </Typography>
+      <Button onClick={() => history.push("/register")} variant="contained">
+        Sign Up
+      </Button>
+      <Link onClick={() => history.push("/login")} variant="body2">
+        {"Already Have An Account? Sign In"}
+      </Link>
+    </Container>
   );
 };
 
